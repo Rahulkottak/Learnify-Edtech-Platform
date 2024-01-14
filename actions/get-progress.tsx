f4a2c0ac0097769f5d336b814192af968d538b83
Rahul Kottak <rahulkottak@gmail.com>
2024-01-14 20:25:12 +0530

    Added course card for student

---
+++ actions/get-progress.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/lib/db";
+
+export const getProgress = async (
+    userId: string,
+    courseId: string,
+): Promise<number>  => {
+
+    try {
+        const publishedChapter = await db.chapter.findMany({
+            where: {
+                courseId,
+                isPublished: true
+            },
+            select: {
+                id: true,
+            }
+        })
+
+        const publishedChapterId = publishedChapter.map((chapter) => chapter.id);
+
+        const validCompletedChapters = await db.userProgress.count({
+            where: {
+                userId: userId,
+                chapterId: {
+                    in: publishedChapterId
+                },
+                isCompleted: true,
+            }
+        })
+
+        const progressPercentage = ( validCompletedChapters / publishedChapterId.length ) * 100;
+
+        return progressPercentage;
+        
+    } catch (error) {
+        console.log("[GET_PROGRESS]", error);
+        return 0;
+    }
+
+}
